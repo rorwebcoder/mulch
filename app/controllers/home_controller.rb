1cd73fac5d963a279d1852597ffcd113a7317eb0
Vadivelan <reachme.vadivelan@gmail.com>
2014-05-20 22:43:54 +0530

    Rails application with fiverr layout

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+		def demo
+				
+		end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mulch::Application.config.secret_token = '79fef53fd8958c6770bbde41df5de4444417293c417f365134489171dffb9afcf8c0c0e09feacc8e5f7baca029bd573907a0c80a1f5582665ae846e2627e1e7b'
