6619169fc38efbe135daee1e581fb4d386f46ce5
udhaya <ror.webcoder@gmail.com>
2014-06-01 22:30:52 -0700

    display in the categories tab modified

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+
+		def categories_home_display
+				categories_display = Category.where('is_public = true').limit(7)
+				categories_display
+		end
+		
+		def categories_home_more
+				categories_more = Category.where('is_public = true')
+				categories_more = categories_more - categories_home_display
+		end
+		
+		def categories_count
+				categories_cnt = Category.where('is_public = true').count
+				categories_cnt
+		end
+		
 end
