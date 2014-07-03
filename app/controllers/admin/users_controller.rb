2167681d4fd009725b1dd354ab3f0fdc0ff82094
rorwebcoder <reachme.vadivelan@gmail.com>
2014-07-03 07:06:19 +0530

    Username as link in service listing page. Show page added for user. Admin panel

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,9 +1,13 @@
 class Admin::UsersController < ApplicationController
 		layout 'admin_layout'		
 		before_filter :custom_auth_user
+		before_filter :valid_record, :only => [:show]
 		
 		def index
 				@users =  User.find(:all , :order=> 'created_at desc')
+		end
+		
+		def show
 		end
 		
 		private
@@ -14,5 +18,10 @@ class Admin::UsersController < ApplicationController
 				elsif current_user.role != "admin"
 						redirect_to home_index_path
 				end
+		end
+		
+		def valid_record
+				return redirect_to admin_users_path if !User.exists?(params[:id])
+				@user = User.find(params[:id])
 		end
 end
