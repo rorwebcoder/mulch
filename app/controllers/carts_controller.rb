e196a28bfa9ae8e6cd7a5c280fb502ead4a5bb90
rorWebCoder <ror.webcoder@gmail.com>
2014-08-15 15:25:18 +0530

    Commented out carts#create action

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,13 +1,13 @@
 class CartsController < ApplicationController
 		def create
-				puts params.inspect
-				"service_count"=>"200",
-				"extra_charges_id"=>["2", "3"],
-				"extra_charges_count"=>{"2"=>"30", "3"=>"10"},
-				"extra_charges_cost"=>{"2"=>"10", "3"=>"10"},
-				"service_id"=>"5"
-				cart = Cart.create()
-				cart.
+				#~ puts params.inspect
+				#~ "service_count"=>"200",
+				#~ "extra_charges_id"=>["2", "3"],
+				#~ "extra_charges_count"=>{"2"=>"30", "3"=>"10"},
+				#~ "extra_charges_cost"=>{"2"=>"10", "3"=>"10"},
+				#~ "service_id"=>"5"
+				#~ cart = Cart.create()
+				#~ cart.
 		end
 		
 		def checkout
